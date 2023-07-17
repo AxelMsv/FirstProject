ccec5974e5bf2b89b63542db0f7601296f4e01bb
Axel Msv <expressmensmv@mail.ru>
2023-07-17 23:40:23 +0300

    Доработал процесс авторизации

--- CreateRequest.php
+++ CreateRequest.php
@@ -4,29 +4,32 @@ class CreateRequest
 
     public $file = "credentials.txt";
 
-    public function setFileName($name) {
+    public function setFileName($name)
+    {
         $this->file = $name;
     }
 
-    public function getParam ($name) {
-        $f = fopen($this->file, "c+") or die ("Не могу открыть файл для чтения!");
+    public function getParam($name)
+    {
+        $f = fopen($this->file, "c+") or die("Не могу открыть файл для чтения!");
         flock($f, LOCK_EX);
         $str = explode("\n", fread($f, 10000));
-        foreach($str as $k=>$v) {
+        foreach ($str as $k => $v) {
             if (strpos($v, $name) === 0) {
                 preg_match('/\s\w\S+/', $v, $match);
                 return trim($match[0]);
             }
         }
-        fclose($f); 
+        fclose($f);
     }
 
-    public function setParam ($name, $param) {
-        $f = fopen($this->file, "c+") or die ("Не могу открыть файл для записи!");   
-        flock($f, LOCK_EX);   
+    public function setParam($name, $param)
+    {
+        $f = fopen($this->file, "c+") or die("Не могу открыть файл для записи!");
+        flock($f, LOCK_EX);
         $str = explode("\n", fread($f, 10000));
         $add = true;
-        foreach($str as $k=>$v) {
+        foreach ($str as $k => $v) {
             if (strpos($v, $name) === 0) {
                 $str[$k] = "$name = $param";
                 $add = false;
@@ -36,61 +39,98 @@ class CreateRequest
             $str[] = "$name = $param";
         }
         ftruncate($f, 0);
-        fseek($f, 0, SEEK_SET);   
-        fwrite($f, implode("\n", $str));  
-        fclose($f); 
-        }   
-
-    public function createUrl ($requestType) { 
-        if (strtolower($requestType) == "access") {
-            return self::getParam('domain').'oauth2/access_token';
-        }
-        if (strtolower($requestType) == "contacts") {
-            return self::getParam('domain').'api/v4/contacts';
-        }   
-        if (strtolower($requestType) == "leads") {
-            return self::getParam('domain').'api/v4/leads';
-        }  
+        fseek($f, 0, SEEK_SET);
+        fwrite($f, implode("\n", $str));
+        fclose($f);
     }
 
-    public function send_curl($url, $data, $headers) {
+    public function getAccess($code)
+    {
+        $url = self::getParam('domain') . 'oauth2/access_token';
+        $data = [
+            'client_id' => self::getParam('client_id'),
+            'client_secret' => self::getParam('client_secret'),
+            'grant_type' => 'authorization_code',
+            'code' => $code,
+            'redirect_uri' => 'http://expressmen.temp.swtest.ru/', // переписать домен +++++++++++
+        ];
+        $headers = [
+            "Content-Type: application/json"
+        ];
+        $result = self::send_curl($url, $data, $headers);
+        if ($result) {
+            self::setParam('access_token', $result['access_token']);
+            self::setParam('refresh_token', $result['refresh_token']);
+            self::setParam('expires_in', $result['expires_in'] + time());
+            self::setParam('status', "true");
+        }
+    }
+
+    public function getRefresh()
+    {
+        $url = self::getParam('domain') . 'oauth2/access_token';
+        $data = [
+            'client_id' => self::getParam('client_id'),
+            'client_secret' => self::getParam('client_secret'),
+            'grant_type' => 'refresh_token',
+            'refresh_token' => self::getParam('refresh_token'),
+            'redirect_uri' => 'http://expressmen.temp.swtest.ru/',
+        ];
+        $headers = [
+            "Content-Type: application/json"
+        ];
+        $response = self::send_curl($url, $data, $headers);
+        self::setParam('access_token', $response['access_token']);
+        self::setParam('refresh_token', $response['refresh_token']);
+        self::setParam('expires_in', $response['expires_in'] + time());
+    }
+
+    public function createUrl($requestType)
+    {
+        if (strtolower($requestType) == "contacts") {
+            return self::getParam('domain') . 'api/v4/contacts';
+        }
+        if (strtolower($requestType) == "leads") {
+            return self::getParam('domain') . 'api/v4/leads';
+        }
+    }
+
+    public function send_curl($url, $data, $headers)
+    {
         $curl = curl_init();
-        curl_setopt($curl,CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-oAuth-client/1.0');
-        curl_setopt($curl,CURLOPT_URL, $url);
-        curl_setopt($curl,CURLOPT_HTTPHEADER,$headers);
-        curl_setopt($curl,CURLOPT_HEADER, false);
-        curl_setopt($curl,CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($curl,CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($curl,CURLOPT_SSL_VERIFYPEER, 1);
-        curl_setopt($curl,CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-oAuth-client/1.0');
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
         $out = curl_exec($curl);
         $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
         curl_close($curl);
         $code = (int)$code; // преобразуем код в целое число
-$errors = [ //создаем список ошибок и их описания
-    400 => 'Bad request',
-    401 => 'Unauthorized',
-    403 => 'Forbidden',
-    404 => 'Not found',
-    500 => 'Internal server error',
-    502 => 'Bad gateway',
-    503 => 'Service unavailable',
-];
+        $errors = [ //создаем список ошибок и их описания
+            400 => 'Bad request',
+            401 => 'Unauthorized',
+            403 => 'Forbidden',
+            404 => 'Not found',
+            500 => 'Internal server error',
+            502 => 'Bad gateway',
+            503 => 'Service unavailable',
+        ];
 
-try
-{
-    /** Если код ответа не успешный - возвращаем сообщение об ошибке  */
-    if ($code < 200 || $code > 204) {
-        throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undefined error', $code);
+        try {
+            /** Если код ответа не успешный - возвращаем сообщение об ошибке  */
+            if ($code < 200 || $code > 204) {
+                throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undefined error', $code);
+            }
+        } catch (Exception $e) {
+
+            self::setParam('status', 'false');
+            die('Ошибка: ' . $e->getMessage() . PHP_EOL . 'Код ошибки: ' . $e->getCode());
+        }
+        return json_decode($out, true);
     }
 }
-catch(Exception $e)
-{
-    self::setParam('status', 'false');
-    die('Ошибка: ' . $e->getMessage() . PHP_EOL . 'Код ошибки: ' . $e->getCode());
-}
-return json_decode($out, true);
-}
-}
-?>
--- GetAccess.php
+++ GetAccess.php
@@ -13,27 +13,10 @@ if (isset($_REQUEST['createRequestButton'])) {
     $request->setParam('client_secret', $_POST['client_secret']);
 
     // создаю запрос на подключение и получение токенов
-    $url = $request->createUrl('access');
-    $data = [
-        'client_id' => $request->getParam('client_id'),
-        'client_secret' => $request->getParam('client_secret'),
-        'grant_type' => 'authorization_code',
-        'code' => $_POST['code'],
-        'redirect_uri' => 'http://expressmen.temp.swtest.ru/', // переписать домен +++++++++++
-    ];
-    $headers = [
-        "Content-Type: application/json"
-    ];
-    $result = $request->send_curl($url, $data, $headers);
+    $request->getAccess($_POST['code']);
 
-    // если подключение успешно, то данные записываются в файл для будущих подключений
-    if ($result) {
-        $request->setParam('access_token', $result['access_token']);
-        $request->setParam('refresh_token', $result['refresh_token']);
-        $request->setParam('expires_in', $result['expires_in'] + time());
-        $request->setParam('status', "true");
-    }
-    header("Location: ./CheckAndSend.php"); // перехожу на страницу отправки заявок
+    // перехожу на страницу отправки заявок
+    header("Location: ./CheckAndSend.php");
 }
 
 //переход на страницу проверки подключения и отправки заявок
@@ -43,109 +26,93 @@ if (isset($_REQUEST['checkButton'])) header("Location: ./CheckAndSend.php");
 if (isset($_REQUEST['sendButton'])) {
 
     $request = new CreateRequest;
-    $url = $request->createUrl('contacts');
+
     // обновляю токен, если просрочен
     if (time() >= $request->getParam('expires_in')) {
-        $url = $request->createUrl('access');
-        $data = [
-            'client_id' => $request->getParam('client_id'),
-            'client_secret' => $request->getParam('client_secret'),
-            'grant_type' => 'refresh_token',
-            'refresh_token' => $request->getParam('refresh_token'),
-            'redirect_uri' => 'http://expressmen.temp.swtest.ru/',
-        ];
-        
-        $headers = [
-            "Content-Type: application/json"
-        ];
-        
-        $response = $request->send_curl($url, $data, $headers);
-        
-        $request->setParam('access_token', $response['access_token']);
-        $request->setParam('refresh_token', $response['refresh_token']); //Refresh токен
-        $request->setParam('expires_in', $response['expires_in'] + time());
+        $request->getRefresh();
     }
 
-//собираем данные с формы 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
-    $email = $_POST['email'];
-    $price = $_POST['price'];
-}
+    //собираем данные с формы 
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $price = $_POST['price'];
+    }
 
-//отправление контакта
-$url = $request->createUrl('contacts');
+    //отправление контакта
+    $url = $request->createUrl('contacts');
 
-$contact_array = array(
-    [
-        "name"=> $name,
-        "first_name" => $name,
-        "custom_fields_values" => [
-            [
-            'field_id'=> 1402705,
-            "values" => [
+    $contact_array = array(
+        [
+            "name" => $name,
+            "first_name" => $name,
+            "custom_fields_values" => [
                 [
-                'value' => $phone
+                    'field_id' => 1402705,
+                    "values" => [
+                        [
+                            'value' => $phone
+                        ]
+                    ]
+                ],
+                [
+                    'field_id' => 1402707,
+                    "values" => [[
+                        'value' => $email
+                    ]]
+                ]
             ]
-            ]
+        ]
+    );
+
+    $headers = [
+        "Accept: application/json",
+        'Authorization: Bearer ' . $request->getParam('access_token')
+    ];
+
+    $Response = $request->send_curl($url, $contact_array, $headers);
+    $contact_id = $Response["_embedded"]["contacts"][0]['id'];
+    echo "Создан контакт: {$contact_id}</br>";
+
+    //отправляем заявку
+    $url = $request->createUrl('leads');
+    $roistat = isset($_COOKIE['roistat_visit']) ? $_COOKIE['roistat_visit'] : 'nocookie';
+    $lead_array = array(
+        [
+            "price" => (int)$price,
+            "custom_fields_values" => [
+                [
+                    "field_id" => 1402725,
+                    "values" => [
+                        [
+                            "value" => $roistat
+                        ]
+                    ]
+                ]
             ],
-            [
-                'field_id'=> 1402707,
-                "values" => [[
-                    'value' => $email
-                ]]
-            ]
-    ]]
-);
-
-$headers = [
-    "Accept: application/json",
-    'Authorization: Bearer ' . $request->getParam('access_token')
-];
-
-$Response = $request->send_curl($url,$contact_array,$headers);
-$contact_id = $Response["_embedded"]["contacts"][0]['id'];
-echo "Создан контакт: {$contact_id}</br>";
-
-//отправляем заявку
-$url = $request->createUrl('leads');
-$roistat = isset($_COOKIE['roistat_visit']) ? $_COOKIE['roistat_visit'] : 'nocookie';
-$lead_array = array(
-    [
-        "price" => (int)$price,
-        "custom_fields_values" => [
-            [
-                "field_id" => 1402725,
-                "values" => [
+            "_embedded" => [
+                "contacts" => [
                     [
-                        "value" => $roistat
+                        "id" => $contact_id
                     ]
                 ]
             ]
-        ],
-        "_embedded" => [ 
-            "contacts"=>[
-            [
-                "id" => $contact_id
-            ]
-            ]
-    ]
-    ]
-);
+        ]
+    );
 
-$headers = [
-    "Accept: application/json",
-    'Authorization: Bearer ' . $request->getParam('access_token')
-];
+    $headers = [
+        "Accept: application/json",
+        'Authorization: Bearer ' . $request->getParam('access_token')
+    ];
 
-$Response = $request->send_curl($url,$lead_array,$headers);
-$lead_id = $Response["_embedded"]["leads"][0]['id'];
-echo "Создана заявка: {$lead_id}<br>";
-echo "Номер визита: {$roistat}";
+    $Response = $request->send_curl($url, $lead_array, $headers);
+    $lead_id = $Response["_embedded"]["leads"][0]['id'];
+    echo "Создана заявка: {$lead_id}<br>";
+    echo "Номер визита: {$roistat}";
 }
 ?>
 
 <form class="back" id="back" method="post" action="./CheckAndSend.php">
-<input class="backButton" name="backButton" type="submit" value="Назад">
+    <input class="backButton" name="backButton" type="submit" value="Назад">
 </form>
